294444dfaca967e9b3ef6d83c1c31d4df079b49c
Evan Zhang <zhang21evan@ncssm.edu>
2020-10-29 21:50:28 -0400

    Commit

--- sojiweb/models.py
+++ sojiweb/models.py
@@ -17,3 +17,8 @@ class EmailList(db.Model):
     first_name = db.Column(db.String(20), nullable=False)
     last_name = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, first_name, last_name, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
