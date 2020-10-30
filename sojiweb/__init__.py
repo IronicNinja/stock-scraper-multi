6a7b9367b125153ac5184591645764019b9d6019
Evan Zhang <zhang21evan@ncssm.edu>
2020-10-29 20:42:00 -0400

    Commit

--- sojiweb/__init__.py
+++ sojiweb/__init__.py
@@ -3,9 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
 import os
-from .models import RealMessage, EmailList
-import click
-from flask.cli import with_appcontext
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -14,10 +11,5 @@ app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 mail = Mail()
 mail.init_app(app)
-
-@click.command(name='create_tables')
-@with_appcontext
-def create_tables():
-    db.create_all()
 
 from sojiweb import routes
