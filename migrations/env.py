261b4dfa3848b11701fe086ddd3602cfddd8a580
Evan Zhang <zhang21evan@ncssm.edu>
2020-10-29 22:04:03 -0400

    Commit

--- migrations/env.py
+++ migrations/env.py
@@ -8,6 +8,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
+# pylint: skip-file
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
