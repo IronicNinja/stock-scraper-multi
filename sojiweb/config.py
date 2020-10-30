004d3795649841756d5e9aeb00d9bf26bad9e753
Evan Zhang <zhang21evan@ncssm.edu>
2020-10-29 21:53:36 -0400

    Commit

--- sojiweb/config.py
+++ sojiweb/config.py
@@ -1,7 +1,7 @@
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_NAVY_URL')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
 MAIL_USE_SSL = True
