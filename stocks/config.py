804b09e1c5542967f659dd2b732ddb21cb018952
Evan Zhang <zhang21evan@ncssm.edu>
2020-11-03 21:03:15 -0500

    Commit

--- stocks/config.py
+++ stocks/config.py
@@ -1,5 +1,9 @@
 import os
 from dotenv import load_dotenv
+from stocks import dev
 load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+if dev:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+else:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
