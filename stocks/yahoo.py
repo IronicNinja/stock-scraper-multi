f763b5f11d3f1f9d78392eb2565560058975337c
Evan Zhang <zhang21evan@ncssm.edu>
2020-11-04 10:11:09 -0500

    Commit

--- stocks/yahoo.py
+++ stocks/yahoo.py
@@ -20,9 +20,6 @@ def get_stocks(stock, start_dt, end_dt):
         for stocks in stock_list:
             stocks = stocks.replace(" ", "")
             df = pdr.data.get_data_yahoo(stocks, start=start_dt, end=end_dt)
-            #path = r'C:\Users\evanz\Desktop\stock_scraper\stocks\downloads'
-            #df.to_csv(os.path.join(path, r'%s.csv' % stocks))
-            #zipObj.write(path, basename(filePath))
             df.to_csv('%s.csv' % stocks)
             zipObj.write('%s.csv' % stocks)
 
@@ -30,6 +27,5 @@ def get_stocks(stock, start_dt, end_dt):
         for stocks in stock_list:
             stocks = stocks.replace(" ", "")
             os.remove('%s.csv' % stocks)
+            
     return random_int
-
-get_stocks('FB, GOOG', '2020-01-05', '2020-01-20')
