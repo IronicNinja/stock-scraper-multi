135326ab3d43a53119f30c01a22e6c268c9a11ab
Evan Zhang <zhang21evan@ncssm.edu>
2020-11-03 21:27:06 -0500

    commit

--- stocks/yahoo.py
+++ stocks/yahoo.py
@@ -5,6 +5,7 @@ import pandas_datareader as pdr
 import os
 from zipfile import ZipFile
 from os.path import basename
+from stocks import dev
 
 def get_stocks(stock, start_dt, end_dt): 
     """
@@ -23,9 +24,9 @@ def get_stocks(stock, start_dt, end_dt):
             #zipObj.write(path, basename(filePath))
             df.to_csv('%s.csv' % stocks)
             zipObj.write('%s.csv' % stocks)
-
-    for stocks in stock_list:
-        stocks = stocks.replace(" ", "")
-        os.remove('%s.csv' % stocks)
+    if dev:
+        for stocks in stock_list:
+            stocks = stocks.replace(" ", "")
+            os.remove('%s.csv' % stocks)
 
 get_stocks('FB, GOOG', '2020-01-05', '2020-01-20')
