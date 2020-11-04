41d0eae6e556a0173c00f1277c33ce59e63212a3
Evan Zhang <zhang21evan@ncssm.edu>
2020-11-04 18:22:03 -0500

    Commit

--- stocks/config.py
+++ stocks/config.py
@@ -4,10 +4,11 @@ from stocks import dev
 import redis
 load_dotenv()
 
+SESSION_TYPE = 'redis'
 if dev:
     SECRET_KEY = os.getenv('SECRET_KEY')
 else:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SESSION_REDIS = redis.from_url(os.environ.get('SESSION_REDIS'))
 
-SESSION_TYPE = 'redis'
-SESSION_REDIS = redis.from_url(os.getenv('SESSION_REDIS'))
+
